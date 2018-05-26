b3b8c1aa0cead94f59edbb384eb313b81c82305b
unknown <akshay.saini@eaglecrk.com>
2018-05-25 23:18:12 -0400

    terminal commands

---
+++ terminal-commands.js
@@ -0,0 +1,30 @@
+console.log('starting terminal commands');
+
+
+
+const fs = require('fs');
+
+module.exports.ls = () => {
+  fs.readdir('./', (err, files) => {
+    const filesToString = files.reduce((acc, file) => {
+     return `${acc} ${file} `;
+   }, '');
+
+   console.log(filesToString);
+  });
+};
+
+module.exports.touch = (err) => {
+  fs.writeFile('message.txt',  'utf8', (err) => {
+  if (err) throw err;
+
+  console.log('success')
+  });
+};
+
+module.exports.mkdir = () => {
+  fs.mkdir('./terminal-commands', (err, files) => {
+    if (err) throw err;
+  console.log('directory created');
+  })
+};
